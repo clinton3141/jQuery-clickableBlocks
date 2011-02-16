e6ca0d74c1a9bc8f4d5b36c2227518e8bac5343f
Clinton Montague <clinton@slightlymore.co.uk>
2011-02-16 15:35:52 -0800

    Formatting and invoked with jQuery instead of assuming global $===jQuery.

--- jQuery.clickableBlocks.js
+++ jQuery.clickableBlocks.js
@@ -1,15 +1,16 @@
-$.fn.clickableBlocks = function ()
-{
-	return this.each (function () {
-		var $this = $(this),
-			$anchor = $this.find("a");
-		// only apply behaviour if there is a single link otherwise it's ambiguous
-		if ($anchor.length == 1)
-		{
-			$this.css("cursor", "pointer").click (function ()
+(function ($) {
+	$.fn.clickableBlocks = function () {
+		return this.each (function () {
+			var $this = $(this),
+				$anchor = $this.find("a");
+			// only apply behaviour if there is a single link otherwise it's ambiguous
+			if ($anchor.length === 1)
 			{
-				location.href = $anchor[0].href;
-			});
-		}
-	});
-}
+				$this.css("cursor", "pointer").click (function ()
+				{
+					location.href = $anchor[0].href;
+				});
+			}
+		});
+	};
+})(jQuery);
